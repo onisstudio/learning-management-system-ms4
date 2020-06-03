414dd81e70ee29dc7fc92a0ea7fd9a0950b8859a
Bogdan Prăzniciuc <bogdan@onis.studio>
2020-06-03 19:24:44 +0100

    Added toasts for messages

--- learning_management_system/cart/views.py
+++ learning_management_system/cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from django.contrib import messages
+
+from courses.models import Course
 
 # Create your views here.
 
@@ -12,11 +15,13 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add item to cart """
 
+    course = get_object_or_404(Course, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     cart[item_id] = 1
 
+    messages.success(request, f'{course.title} added to the cart')
     request.session['cart'] = cart
 
     return redirect(redirect_url)
@@ -25,9 +30,17 @@ def add_to_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Remove the item from cart"""
 
-    print('test')
-    cart = request.session.get('cart', {})
-    cart.pop(item_id)
+    try:
+        course = get_object_or_404(Course, pk=item_id)
+        cart = request.session.get('cart', {})
 
-    request.session['cart'] = cart
-    return render(request, 'cart/cart.html')
+        cart.pop(item_id)
+
+        messages.success(request, f'{course.title} removed from the cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing course: {e}')
+        return HttpResponse(status=500)
