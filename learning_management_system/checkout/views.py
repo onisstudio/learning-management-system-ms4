7dd7e193b370b3927afb6c5b9c216df87e114444
Bogdan Prăzniciuc <bogdan@onis.studio>
2020-06-11 11:31:38 +0100

    Updated checkout success template

--- learning_management_system/checkout/views.py
+++ learning_management_system/checkout/views.py
@@ -55,7 +55,8 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_cart'))
 
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
