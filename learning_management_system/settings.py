0746e9b576ac3cb983c865ab20e2890a20289a84
Bogdan Prăzniciuc <bogdan@onis.studio>
2020-06-16 21:51:48 +0100

    Update settings.py

--- learning_management_system/settings.py
+++ learning_management_system/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lms-ms4.herokuapp.com', 'localhost']
 
 # Application definition
 
